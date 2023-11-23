fcccca0709c443a274e1d136bf278b216331f141
johann <johannzr17@gmail.com>
2023-11-23 16:24:45 -0500

    Crear repository y el metodo show de producto controller

---
+++ src/main/java/com/cibertec/controller/Productocontroller.java
@@ -0,0 +1,17 @@
+package com.cibertec.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/productos")
+public class Productocontroller {
+
+	@GetMapping("")
+	public String show() {
+		return "productos/show";
+	}
+}
