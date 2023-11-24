e0e09c31e97061de99a65df5e4671efe24cbf361
johann <johannzr17@gmail.com>
2023-11-24 11:26:05 -0500

    implementar las funcionalidades de cargar imagen y mostar para el administrador

--- src/main/java/com/cibertec/LenguajeIiProyectoFinalApplication.java
+++ src/main/java/com/cibertec/LenguajeIiProyectoFinalApplication.java
@@ -1,12 +1,10 @@
 package com.cibertec;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LenguajeIiProyectoFinalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LenguajeIiProyectoFinalApplication.class, args);
 	}
--- src/main/java/com/cibertec/controller/Productocontroller.java
+++ src/main/java/com/cibertec/controller/Productocontroller.java
@@ -1,6 +1,8 @@
 package com.cibertec.controller;
 
+import java.io.IOException;
 import java.util.Optional;
+
 
 
 import org.slf4j.*;
@@ -13,9 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.cibertec.model.Producto;
 import com.cibertec.model.Usuario;
 import com.cibertec.service.ProductoService;
+import com.cibertec.service.UploadFileService;
+
 
 @Controller
 @RequestMapping("/productos")
@@ -26,6 +33,9 @@ public class Productocontroller {
 	@Autowired
 	private ProductoService productoService;
 	
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@GetMapping("")
 	public String show(Model model) {
@@ -38,11 +48,19 @@ public class Productocontroller {
 		return "productos/create";
 	}
 	@PostMapping("/save")
-	public String save(Producto producto){
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
 		
 		LOGGER.info("este es el objeto producto{}", producto);
 		Usuario u= new Usuario(1, "","","","","","","");
 		producto.setUsuario(u);	
+		
+		//imagen
+		if (producto.getId()==null) { // cuando se crea un producto
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}else {
+					
+		}
 		
 		productoService.save(producto);
 		return "redirect:/productos";
@@ -60,15 +78,37 @@ public class Productocontroller {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto){
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
+		Producto p= new Producto();
+		p=productoService.get(producto.getId()).get();
 		
+		if (file.isEmpty()) { // editamos el producto pero no cambiamos la imagem
+			
+			producto.setImagen(p.getImagen());
+		}else {// cuando se edita tbn la imagen	
+
+			//eliminar cuando no sea la imagen por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+
+			String nombreImagen= upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);		
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {		
+		Producto p = new Producto();
+		p=productoService.get(id).get();
 
+		//eliminar cuando no sea la imagen por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
 		productoService.delete(id);
 		return "redirect:/productos";
 	}
