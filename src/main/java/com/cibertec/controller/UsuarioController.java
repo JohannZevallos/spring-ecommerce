56e97437a12513b19e9b8d3001bf3e359c92ce03
johann <johannzr17@gmail.com>
2023-11-26 17:52:47 -0500

    mostrar la vista compras para el user

--- src/main/java/com/cibertec/controller/UsuarioController.java
+++ src/main/java/com/cibertec/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.cibertec.controller;
 
-
 import java.util.Optional;
 
 
@@ -9,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -71,4 +71,12 @@ public class UsuarioController {
 		return "redirect:/";
 	}
 	
+	@GetMapping("/compras")
+	public String obtenerCompras(Model model, HttpSession session) {
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
+		
+		
+		return "usuario/compras";
+	}
 }
